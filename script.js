f77fcb34eb9f64c7d81070b5e72e5dc6eb16404f
Jagat Singh <126696595+jagat-singh-chaudhary@users.noreply.github.com>
2024-11-09 16:39:19 +0530

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyAlOLQM9cz1kyys9vlvONRWi9CwLcpp6qE"; // Your API key here
+const API_KEY = "AIzaSyAVuiKJego_3I6DPoMSpoEpmls45JM6ypE"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
